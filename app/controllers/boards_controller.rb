0cc82982fc8dd201c976c789e71de8288989f6ec
yukten <yuk7829@gmail.com>
2025-03-08 16:37:48 +0900

    記事を更新できるように

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -21,6 +21,20 @@ class BoardsController < ApplicationController
     end
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to root_path, notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
   private
   def board_params
     params.require(:board).permit(:name, :description)
